c0591e426fbef351337e12d19ff360d770df771b
lekiam2023 <27883maikel@gmail.com>
2025-07-11 19:14:09 +0200

    Inicializacion backend con dependencias y estructura

---
+++ utils/jwt.js
@@ -0,0 +1,7 @@
+const jwt = require('jsonwebtoken');
+const secret = process.env.JWT_SECRET;
+
+const createToken = (payload) => jwt.sign(payload, secret, {expiresIn: '1d'});
+const verifyToken = (token) => jwt.verify(token, secret);
+
+module.exports = {createToken, verifyToken};
