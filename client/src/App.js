ce18c056fb8ed2b7b1a61003759d35d288b05ed1
lekiam2023 <27883maikel@gmail.com>
2025-07-25 13:12:21 +0200

    feat(frontend): actualizaciones en dependencias y nuevas rutas/páginas

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,20 @@
 
-import { useState } from 'react';
+import { useState, useEffect, use } from 'react';
 import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
-
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
-   const [showLogin, setShowLogin] = useState(true);
   return (
-    <div style={{maxWidth: '400px', margin: 'auto'}}>
-
-      <h1>{showLogin ? 'Iniciar Sesión': 'Registrarse'}</h1>
-
-      {showLogin ? <LoginForm /> : <RegisterForm />}
-      
-      <button onClick={() => setShowLogin(!showLogin)} style={{ marginTop:'20px' }}>
-         {showLogin ? 'No tienes cuenta? Registrate' : 'Ya ¿Tienes cuenta Inicia Sesión?'}
-      </button>
-    </div>
+      <Router>
+         <Routes>
+            <Route path="/" element={<Navigate to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>}/>
+         </Routes>
+      </Router> 
   );
 }
 
