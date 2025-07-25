c74257b5630a41adb5e97d14a7352015edde904d
lekiam2023 <27883maikel@gmail.com>
2025-07-25 13:43:54 +0200

    Creamos componente para el registro de usuarios

---
+++ client/src/pages/Register.js
@@ -0,0 +1,12 @@
+import RegisterForm from "../components/RegisterFormForm";
+
+function Register(){
+    return(
+        <div style={{maxWidth:'400px', margin:'auto'}}>
+            <h1>Registro</h1>
+            <RegisterForm />
+        </div>
+    );
+}
+
+export default Register;
