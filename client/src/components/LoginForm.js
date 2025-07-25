f7f42073f2d37b118ed1f0963d2f3bc6334397b9
lekiam2023 <27883maikel@gmail.com>
2025-07-25 13:56:18 +0200

    Modificamos el componente LoginForm para redirigir al dashboard

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,58 +1,32 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import API from '../services/api';
 
 function LoginForm(){
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [mensaje, setMensaje] = useState('');
     const [error, setError] = useState('');
+    const navigate = useNavigate();
 
     const handleLogin = async (e) => {
         e.preventDefault();
-        setMensaje('');
         setError('');
      try{
         const res = await API.post('/api/login', {email, password, });
-
         //Guardamos token en LocalStorage
         localStorage.setItem('token', res.data.token);
-
-        setMensaje('¡Inicio de sesión exitoso!');
-        console.log('Datos del usuario:', res.data.user);
-
+        navigate('/dashboard');//Redirige al dashboard 
      }catch(err){
         setError('Credenciales invalidas');
      } 
   };
     return (
-        <div style={{maxWidth: 300, margin: 'auto'}}>
-            <h2>Iniciar Sesión</h2>
-            <form onSubmit={handleLogin}>
-             <input 
-                type="email"
-                placeholder="Correo"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-                style={{ width: '100%', marginBottom: '10px' }}
-             />
-
-            <input 
-                 type="password"
-                 placeholder="Contraseña"
-                 value={password}
-                 onChange={(e) => setPassword(e.target.value)}
-                 required
-                 style={{ width: '100%', marginBottom: '10px' }}
-             />
-             <button type="submit" style={{ width: '100%' }}>
-                 Iniciar Sesión
-             </button>
-          </form>
-             {mensaje && <p style={{color: 'green'}}>{mensaje}</p>}
-             {error && <p style={{color: 'red'}}>{error}</p>}
-        </div>
-
+        <form onSubmit={handleLogin}>
+           <input type="email" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder="Correo" required />
+           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" required />
+           <button type="submit">Iniciar Sesíon</button>
+           {error && <p style={{ color:'red' }}>{error}</p>}
+        </form>
      );
 }
 
