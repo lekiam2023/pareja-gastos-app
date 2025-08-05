dac362a838a39548ce35e3fee0247b20caa00323
lekiam2023 <27883maikel@gmail.com>
2025-08-05 13:58:28 +0200

    Mejoramos la apariencia del formulario, en un futuro sera con Tailwind

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -23,15 +23,86 @@ function LoginForm(){
      } 
   };
 
-    return (
-        <form onSubmit={handleLogin}>
-            <input type="name" value={name} onChange={(e) =>setName(e.target.value)} placeholder="Nombre" required />
-           <input type="email" value={email} onChange={(e) =>setEmail(e.target.value)} placeholder="Correo" required />
-           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Contraseña" required />
-           <button type="submit">Iniciar Sesíon</button>
-           {error && <p style={{ color:'red' }}>{error}</p>}
+    return(
+      <div style={styles.container}>
+        <form onSubmit={handleLogin} style={styles.form}>
+          <h2 style={styles.title}>Iniciar Sesión</h2>
+
+           <input
+            type="text" 
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            placeholder="Nombre"
+            required
+            style={styles.input}
+         />
+
+           <input type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Correo electronico"
+            required 
+            style={styles.input}
+         />
+
+           <input type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Contraseña"
+            required
+            style={styles.input}
+          />
+           <button type="submit" style={styles.button}>Entrar</button>
+
+           {error && <p style={styles.error}>{error}</p>}
         </form>
+        </div>
      );
 }
+
+const styles ={
+   container:{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      background: '#f3f4f6',
+   },
+   form:{
+      background: '#fff',
+      padding: '30px',
+      borderRadius: '8px',
+      boxShadow: '0 4px 8px rgba(0,0,0,0.1)',
+      width: '100%',
+      maxWidth: '400px',
+      display: 'flex',
+      flexDirection: 'column',
+   },
+   title:{
+      marginBottom: '2px',
+      textAling: 'center',
+      color: '#333',
+   },
+   input:{
+      padding: '12px',
+      marginBottom: '15px',
+      borderRadius: '5px',
+      border: '1px solid #ccc',
+      fontSize: '16px',
+   },
+   button:{
+      padding: '12px',
+      backgroundColor: '#4f46e5',
+      color: '#fff',
+      border: 'none',
+      borderRadius: '5px',
+      fontSize: '16px',
+      cursor: 'pointer',
+   },
+   error:{
+      marginTop: '10px',
+      color: 'red',
+      textAling: 'center',
+   },
+};
 
 export default LoginForm;
