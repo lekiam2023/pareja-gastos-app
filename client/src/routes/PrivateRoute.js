104963221106047d28dd65b374e82be992db5f89
lekiam2023 <27883maikel@gmail.com>
2025-09-01 14:08:10 +0200

    Dev: permitir acceso al Dashboard sin login en modo desarrollo

--- client/src/routes/PrivateRoute.js
+++ client/src/routes/PrivateRoute.js
@@ -1,9 +1,18 @@
 /*Este componente protegerá rutas que necesitan login (como /dashboard).*/
+import React from 'react';
 import {Navigate} from 'react-router-dom';
 
 const PrivateRoute = ({ children }) =>{
+    const isDev = process.env.NODE_ENV === "development";
     const token = localStorage.getItem('token');
-     return token ? children : <Navigate to="/login" />;
+
+   //En modo desarrollo, siempre permite acceder
+   if(!isDev && !token){
+   return <Navigate to="/login"/>
+   }
+   
+   return children;
+ 
 };
 
 export default PrivateRoute;
